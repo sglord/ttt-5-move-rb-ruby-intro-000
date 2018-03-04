8653d7e9f0f5d92bacb41ab2c9e6dbd76b7c72b6
S L <lordstepheng@gmail.com>
2018-03-04 06:28:18 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +8,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+
 
 # code your input_to_index and move method here!
---
+++ newcolors.rb
@@ -0,0 +1,8 @@
+#colors for practicing Arrays
+
+colors = ["red", "Green", "Blue"]
+
+def coloring_book(colors)
+  puts "#{colors[0]}"
+end
+
