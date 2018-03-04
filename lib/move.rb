7ec3bec457e493643ac4f0a65ef1e1dd79dafef3
S L <lordstepheng@gmail.com>
2018-03-04 06:58:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,9 +10,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 display_board(board)
-
-
+puts "turn 1"
 puts "Where would you like to go?"
 position = gets.strip
 player = "x"
