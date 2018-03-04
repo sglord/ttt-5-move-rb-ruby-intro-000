8cfbad4d1f9281e880cecd9afed3c35bb9d2d0e7
S L <lordstepheng@gmail.com>
2018-03-04 06:56:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,6 @@
 board = [" "," "," "," "," "," "," "," "," "]
 puts "Welcome to Tic Tac Toe!"
-
+display_board(board)
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
