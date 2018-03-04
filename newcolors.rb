cdbb7875483f9b50b96536632c1d26c8853f0804
S L <lordstepheng@gmail.com>
2018-03-04 06:40:40 +0000

    automatically backed up by learn

--- newcolors.rb
+++ newcolors.rb
@@ -13,12 +13,11 @@ colors = ["red", "Green", "Blue"]
 puts "These are my favorite colors!"
 puts colors
 puts "What is your new fav color?"
+
 new_fav = gets.strip
 #new_fav=red
 #how to make colors[1]=new_fav=red
-
-
-#colors[new_fav]
+colors[new_fav]
 
 
 
